ee4b15e9c11f70677c35f2b8ccbfb6d52779cd70
adammakai8 <adammakai8@gmail.com>
2023-03-08 19:50:02 +0100

    Project skeleton

---
+++ src/main/java/edu/maszek/brainpowerquiz/BrainpowerquizApplication.java
@@ -0,0 +1,13 @@
+package edu.maszek.brainpowerquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainpowerquizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainpowerquizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/maszek/brainpowerquiz/BrainpowerquizApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.maszek.brainpowerquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainpowerquizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
